52f7a4972f6df7b15badc5b8decb198089757676
Nicholas Fearns <you@example.com>
2019-06-04 00:44:53 +0000

    fixed url typo causing a routing error i  the index.html.erb file

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,13 +10,12 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = response_value.each do |task|
+      response_ids = response_value.collect do |task|
         task["id"]
       end
       expect(response_ids).to eq([task1.id, task2.id])
     end
   end
-
   describe "tasks#update" do
     it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task, done: false)
