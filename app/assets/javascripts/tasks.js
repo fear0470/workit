4fdb8c9e0cbff669ba060d7fe6883d6d9ac5a3b1
Nicholas Fearns <you@example.com>
2019-06-08 22:53:50 +0000

    added strikethrough when a task is completed. modified javascript so it updated whether a task was toggled or not

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,13 +3,15 @@
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
-      var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
-        " data-id='" + task.id + "'" +
-        checkedStatus +
-        '><label>' +
-         task.title +
-         '</label></div></li>';
+    var checkedStatus = task.done ? "checked" : "";
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
 
       return liElement;
     }
@@ -28,7 +30,14 @@
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data)
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+
+
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
