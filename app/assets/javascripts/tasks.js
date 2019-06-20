c2b3c6d50059bef1de9cfdb22f0b8cc3089395c0
Nicholas Fearns <you@example.com>
2019-06-20 23:55:28 +0000

    modified styling

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,8 @@
-  $(function() {
-    // The taskHtml method takes in a JavaScript representation
-    // of the task and produces an HTML representation using
-    // <li> tags
-    function taskHtml(task) {
+$(function() {
+  // The taskHtml method takes in a JavaScript representation
+  // of the task and produces an HTML representation using
+  // <li> tags
+  function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
     var liClass = task.done ? "completed" : "";
     var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
@@ -13,62 +13,60 @@
        task.title +
        '</label></div></li>';
 
-      return liElement;
-    }
+    return liElement;
+  }
 
-    // toggleTask takes in an HTML representation of the
-    // an event that fires from an HTML representation of
-    // the toggle checkbox and  performs an API request to toggle
-    // the value of the `done` field
-    function toggleTask(e) {
-      var itemId = $(e.target).data("id");
+  // toggleTask takes in an HTML representation of the
+  // an event that fires from an HTML representation of
+  // the toggle checkbox and  performs an API request to toggle
+  // the value of the `done` field
+  function toggleTask(e) {
+    var itemId = $(e.target).data("id");
 
-      var doneValue = Boolean($(e.target).is(':checked'));
+    var doneValue = Boolean($(e.target).is(':checked'));
 
-      $.post("/tasks/" + itemId, {
-        _method: "PUT",
-        task: {
-          done: doneValue
-        }
-      }).success(function(data) {
-        var liHtml = taskHtml(data)
-        var $li = $("#listItem-" + data.id);
-        $li.replaceWith(liHtml);
-        $('.toggle').change(toggleTask);
-
-
-      } );
-    }
-
-    $.get("/tasks").success( function( data ) {
-      var htmlString = "";
-
-      $.each(data, function(index,  task) {
-        htmlString += taskHtml(task);
-      });
-      var ulTodos = $('.todo-list');
-      ulTodos.html(htmlString);
-
+    $.post("/tasks/" + itemId, {
+      _method: "PUT",
+      task: {
+        done: doneValue
+      }
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
       $('.toggle').change(toggleTask);
 
-    });
+    } );
+  }
 
+  $.get("/tasks").success( function( data ) {
+    var htmlString = "";
 
-    $('#new-form').submit(function(event) {
-      event.preventDefault();
-      var textbox = $('.new-todo');
-      var payload = {
-        task: {
-          title: textbox.val()
-        }
-      };
-      $.post("/tasks", payload).success(function(data) {
-        var htmlString = taskHtml(data);
-        var ulTodos = $('.todo-list');
-        ulTodos.append(htmlString);
-        $('.toggle').click(toggleTask);
-        $('.new-todo').val('');
-      });
+    $.each(data, function(index,  task) {
+      htmlString += taskHtml(task);
     });
+    var ulTodos = $('.todo-list');
+    ulTodos.html(htmlString);
+
+    $('.toggle').change(toggleTask);
 
   });
+
+
+  $('#new-form').submit(function(event) {
+    event.preventDefault();
+    var textbox = $('.new-todo');
+    var payload = {
+      task: {
+        title: textbox.val()
+      }
+    };
+    $.post("/tasks", payload).success(function(data) {
+      var htmlString = taskHtml(data);
+      var ulTodos = $('.todo-list');
+      ulTodos.append(htmlString);
+      $('.toggle').click(toggleTask);
+    });
+  });
+
+});
